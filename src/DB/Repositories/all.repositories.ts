0a3098812f74aa37adcdd7b670414d5df49c1e51
Ampomah Winston <winstyngyen@gmail.com>
2022-09-02 11:50:31 +0000

    changed location

--- src/DB/Repositories/all.repositories.ts
+++ src/DB/Repositories/all.repositories.ts
@@ -3,7 +3,7 @@ import Contact from "../Entities/contact.entity";
 import Member from "../Entities/members.entity";
 import User from "../Entities/users.entity";
 import attendance from "../Entities/attendance.entity";
-import MgnlCompanyEntity from "../Entities/Magnolia/MgnlCompany.entity";
+import MgnlCompanyEntity from "../Entities/MgnlCompany.entity";
 //repos
 export const MemberRepository = DB.getRepository(Member);
 export const ContactRepository = DB.getRepository(Contact);
